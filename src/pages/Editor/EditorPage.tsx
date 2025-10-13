56176b6f7b7cf988f782412aee72005caa3c7020
Anik Dey <deyanik659@gmail.com>
2025-10-13 12:30:05 +0600

    updated

--- src/pages/Editor/EditorPage.tsx
+++ src/pages/Editor/EditorPage.tsx
@@ -3,7 +3,7 @@ import PdfViewer from "../../features/editor/Editor";
 export default function EditorPage() {
     return (
         <div className="relative rounded bg-white p-6 shadow">
-            <h2 className="mb-4 font-bold text-2xl">Document Title</h2>
+            <h2 className="mb-4 font-bold text-2xl">Document Title Main</h2>
             <PdfViewer />
             <ZoomControls />
         </div>
